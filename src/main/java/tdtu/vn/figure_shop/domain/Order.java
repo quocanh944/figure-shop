f5bbac3492db3243b014ee11ee5e45cc0d424463
Han Nguyen <quocanh944@gmail.com>
2023-08-21 00:02:06 +0700

    Add sample data order and user

--- src/main/java/tdtu/vn/figure_shop/domain/Order.java
+++ src/main/java/tdtu/vn/figure_shop/domain/Order.java
@@ -1,15 +1,7 @@
 package tdtu.vn.figure_shop.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
 import java.util.Set;
 import lombok.Getter;
 import lombok.Setter;
@@ -41,7 +33,7 @@ public class Order {
     @OneToMany(mappedBy = "order")
     private Set<OrderDetail> orderDetails;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.MERGE)
     @JoinColumn(name = "user_id")
     private UserEntity user;
 
