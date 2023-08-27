e6c25134e8f753b90562c08391b791a62d7f3676
nguyenan <tuongankk2003@gmail.com>
2023-08-27 22:42:36 +0700

    email html

--- src/main/java/tdtu/vn/figure_shop/service/EmailService.java
+++ src/main/java/tdtu/vn/figure_shop/service/EmailService.java
@@ -2,9 +2,13 @@ package tdtu.vn.figure_shop.service;
 
 import jakarta.mail.internet.MimeMessage;
 import lombok.AllArgsConstructor;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import java.nio.charset.StandardCharsets;
 
 
 @Service
@@ -19,22 +23,9 @@ public class EmailService {
             helper = new MimeMessageHelper(message, true); // True indicates multipart message
             helper.setTo(recipientEmail);
             helper.setSubject("Order Confirmation");
-            String emailContent = "<html>"
-                    + "<head>"
-                    + "<style>"
-                    + "body { font-family: Arial, sans-serif; }"
-                    + ".header { background-color: #f2f2f2; padding: 10px; }"
-                    + ".content { padding: 20px; }"
-                    + "</style>"
-                    + "</head>"
-                    + "<body>"
-                    + "<div class='header'><h1>Order Confirmation</h1></div>"
-                    + "<div class='content'>"
-                    + "<p>Thank you for your order! Here are your order details:</p>"
-                    + "<pre>" + orderDetails + "</pre>"
-                    + "</div>"
-                    + "</body>"
-                    + "</html>";
+            Resource resource = new ClassPathResource("orderMail.html");
+            String emailContent = new String(resource.getInputStream().readAllBytes(), StandardCharsets.UTF_8);
+            emailContent = emailContent.replace("{ORDER_DETAILS}", orderDetails);
             helper.setText(emailContent,true);
             javaMailSender.send(message);
         } catch (Exception e) {
