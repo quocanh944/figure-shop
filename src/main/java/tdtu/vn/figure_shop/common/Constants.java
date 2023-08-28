1166066a239b56f588a1dabacc3a55b91bd88488
Han Nguyen <quocanh944@gmail.com>
2023-08-28 21:00:07 +0700

    Fix dto create product

--- src/main/java/tdtu/vn/figure_shop/common/Constants.java
+++ src/main/java/tdtu/vn/figure_shop/common/Constants.java
@@ -4,13 +4,13 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class Constants {
-    public static final String FB_SDK_JSON = "example.json"; // Your firebase private json in resource folder
+    public static final String FB_SDK_JSON = "firebase-private.json"; // Your firebase private json in resource folder
 
-    public static final String STORAGE_ID = "<<bucket>>.appspot.com"; // Storage id <<bucket_name>>
+    public static final String STORAGE_ID = "sake-figure-shop-2.appspot.com"; // Storage id <<bucket_name>>
 
-    public static final String FB_PROJECT_ID = ""; // Firebase project id
+    public static final String FB_PROJECT_ID = "sake-figure-shop-2"; // Firebase project id
 
-    public static final String FB_ACCESS_TOKEN = ""; // Token to access file
+    public static final String FB_ACCESS_TOKEN = "sakeaccesstoken"; // Token to access file
 
     public static final String FB_STORAGE_URL = "firebasestorage.googleapis.com/v0/b/" + STORAGE_ID + "/o/";
 
