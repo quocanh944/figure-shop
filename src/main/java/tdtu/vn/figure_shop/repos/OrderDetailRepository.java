957cecfbe79c7a9d46f255a145d4da4d751e3661
Han Nguyen <quocanh944@gmail.com>
2023-08-30 23:54:29 +0700

    Add best seller feature

--- src/main/java/tdtu/vn/figure_shop/repos/OrderDetailRepository.java
+++ src/main/java/tdtu/vn/figure_shop/repos/OrderDetailRepository.java
@@ -3,13 +3,17 @@ package tdtu.vn.figure_shop.repos;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import tdtu.vn.figure_shop.domain.OrderDetail;
+import tdtu.vn.figure_shop.model.ProductCount;
 
 import java.util.List;
 
 
 public interface OrderDetailRepository extends JpaRepository<OrderDetail, Long> {
     @Query("SELECT SUM(od.quantity) FROM OrderDetail od")
-    public Long getAllSoldProduct();
+    Long getAllSoldProduct();
 
-    public List<OrderDetail> findAllByOrder_Id(Long orderId);
+    List<OrderDetail> findAllByOrder_Id(Long orderId);
+
+    @Query("SELECT new tdtu.vn.figure_shop.model.ProductCount(count(od.quantity), od.product.id) FROM OrderDetail od GROUP BY od.product")
+    List<ProductCount> getBestSeller();
 }
